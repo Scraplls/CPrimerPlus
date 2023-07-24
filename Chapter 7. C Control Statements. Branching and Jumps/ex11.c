670c6dea19bd5654e1f2b87a4b4eff965c44695b
scraplls <scrolls.aplles@mail.ru>
2023-07-24 16:27:51 +0300

    third

--- Chapter 7. C Control Statements. Branching and Jumps/ex11.c
+++ Chapter 7. C Control Statements. Branching and Jumps/ex11.c
@@ -15,3 +15,81 @@
  * of the order, the discount (if there is one), the shipping charge, and the grand total of all
  * the charges.
  */
+#include <stdio.h>
+#include <stdbool.h>
+
+#define ARTICHOKES_PRICE 2.05
+#define BEETS_PRICE 1.15
+#define CARROTS_PRICE 1.09
+#define DISCOUNT 0.05
+
+int main(void){
+    char choice;
+    float pounds;
+    float a_pounds, b_pounds, c_pounds;
+    float a_cost, b_cost, c_cost;
+    float cost, discount, shipping, total;
+
+    a_pounds = b_pounds = c_pounds = 0;
+    cost = discount = shipping = total = 0;
+
+    printf("==========================\n");
+    printf("The ABC Mail Order Grocery\n");
+    printf("==========================\n");
+    printf("Order menu: \n");
+    printf("a) artichokes\n");
+    printf("b) beets\n");
+    printf("c) carrots\n");
+    printf("-------------------------\n");
+    printf("q) quit\n");
+
+    while ((scanf("%c", &choice)) && choice != 'q'){
+        printf("Enter weight of product in pounds: ");
+        if(!scanf("%f", &pounds) || pounds < 1e-3)
+            break;
+        switch (choice) {
+            case 'a':
+                a_pounds += pounds;
+                break;
+            case 'b':
+                b_pounds += pounds;
+                printf("Enter weight of beets in pounds: ");
+                break;
+            case 'c':
+                c_pounds += pounds;
+                printf("Enter weight of carrots in pounds: ");
+                break;
+            default:
+                printf("Order menu: \n");
+                printf("a) artichokes\n");
+                printf("b) beets\n");
+                printf("c) carrots\n");
+                printf("-------------------------\n");
+                printf("q) quit\n");
+                break;
+        }
+    }
+
+    pounds = a_pounds + b_pounds + c_pounds;
+    a_cost = a_pounds * ARTICHOKES_PRICE;
+    b_cost = b_pounds * BEETS_PRICE;
+    c_cost = c_pounds * CARROTS_PRICE;
+    cost = a_cost + b_cost + c_cost;
+    discount = cost * DISCOUNT;
+    if(pounds < 5)
+        shipping = 6.50;
+    else if(pounds >= 5 && pounds < 20)
+        shipping = 14.00;
+    else
+        shipping = 14.00 + 0.50 * (pounds - 20);
+    total = cost - discount + shipping;
+    printf("[Receipt]\n");
+    printf("Artichokes          %.2fp. x %.2f$ = %.2f$\n", a_pounds, ARTICHOKES_PRICE, a_cost);
+    printf("Beets          %.2fp. x %.2f$ = %.2f$\n", b_pounds, BEETS_PRICE, b_cost);
+    printf("Carrots          %.2fp. x %.2f$ = %.2f$\n", b_pounds, CARROTS_PRICE, c_cost);
+    printf("Cost             %.2f$\n", b_pounds, CARROTS_PRICE, c_cost);
+
+    printf("----------------------------------------------------------\n");
+
+    return 0;
+}
