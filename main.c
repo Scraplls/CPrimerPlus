0aec0b00c62a8efd9f8391dac50752bd7e637eca
PolkaEL <scrolls.aplles@mail.ru>
2023-09-01 12:36:10 +0300

    new

--- main.c
+++ main.c
@@ -2,14 +2,5 @@
 
 int main(void)
 {
-    char ch;
-    FILE *fp;
-    fp = fopen("readme.md", "r");
-    /* seek for a counter on the last line */
-    fseek(fp, -2, SEEK_END);
-    ch = getc(fp);
-    while (ch != EOF)
-        ch = getc(fp);
-    fclose(fp);
     return 0;
 }
