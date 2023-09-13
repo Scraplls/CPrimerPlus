07f911c3314776f62ef6dc9eced78875f587229c
PolkaEL <scrolls.aplles@mail.ru>
2023-09-13 12:52:17 +0300

    new

--- Chapter 16 The C Preprocessor and the C Library/ex5.c
+++ Chapter 16 The C Preprocessor and the C Library/ex5.c
@@ -7,3 +7,98 @@
  * similar function available, use its output with srand() to initialize the rand() random-
  * number generator. Write a simple program that tests the function.
  */
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "ex1.h"
+
+void printarr(int arr[], int n);
+void randpick(int arr[], int n, int picks);
+bool ispicked(int arr[], int n);
+
+static bool* picked;
+
+int main(void)
+{
+    int i, n, picks;
+    int* array;
+    picks = 0;
+    n = 0;
+
+    printf("Enter the size of an array:\n");
+    scanf("%d", &n);
+    CLEARINPUT;
+    if(n <= 0)
+    {
+        fprintf(stderr, "Error! Size of an array must be greater than 0.");
+        exit(EXIT_FAILURE);
+    }
+    array = (int*) malloc(n * sizeof(int));
+    picked = (bool*) malloc(n * sizeof(bool));
+    for(i = 0; i < n; i++)
+    {
+        picked[i] = false;
+        array[i] = 0;
+    }
+    printf("Enter the contents of an array (e.g. 1 2 3 4 ...):\n");
+    i = 0;
+    while (i < n && scanf("%d", &array[i]))
+        i++;
+    CLEARINPUT;
+
+    printarr(array, n);
+    printf("Enter the number of picks:\n");
+    while(scanf("%d", &picks) && picks != 0)
+    {
+        CLEARINPUT;
+        randpick(array, n, picks);
+        printarr(array, n);
+        if(ispicked(array, n))
+        {
+            printf("There are no elements in the array to be picked!\n");
+            break;
+        }
+        printf("Enter the number of picks:\n");
+    }
+    printf("Bye!\n");
+    return 0;
+}
+
+void printarr(int arr[], int n)
+{
+    printf("Current array: ");
+    for(int i = 0; i < n; i++)
+        printf("%c%c", picked[i] ? '_' : ' ', ' ');
+    printf("\n               ");
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void randpick(int arr[], int n, int picks)
+{
+    int pos, i;
+    srand(clock());
+
+    printf("Picked numbers: ");
+    for(i = 0; i < picks && !ispicked(arr, n);)
+    {
+        pos = rand() % n;
+        if(!picked[pos])
+        {
+            picked[pos] = true;
+            printf("%d, ", arr[pos]);
+            i++;
+        }
+    }
+    printf("count=%d\n", i);
+}
+
+bool ispicked(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+        if(!picked[i])
+            return false;
+    return true;
+}
---
+++ Chapter 16 The C Preprocessor and the C Library/ex6.c
@@ -0,0 +1,64 @@
+/* C Primer Plus. Programming Exercise 16.6
+ * Modify Listing 16.17 so that it uses an array of struct names elements (as defined after
+ * the listing) instead of an array of double. Use fewer elements, and initialize the array
+ * explicitly to a suitable selection of names.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSTAFF 10
+
+typedef struct {
+    char first[40];
+    char last[40];
+} names;
+
+void showarray(const names ar[], int n);
+int mycomp(const void * p1, const void * p2);
+
+int main(void)
+{
+    names staff[MAXSTAFF] = {
+            {"John", "Romero"},
+            {"Akira", "Yamaoka"},
+            {"Walter", "Sullivan"},
+            {"John", "Carmack"},
+            {"Charles", "Xavier"},
+            {"Todd", "Howard"},
+            {"Bob", "Ross"},
+            {"Paul", "Allen"},
+            {"Tyler", "Durden"},
+            {"Mike", "Sully"}
+    };
+
+    puts("List of staff:");
+    showarray(staff, MAXSTAFF);
+    qsort(staff, MAXSTAFF, sizeof(names), mycomp);
+    puts("\nSorted list:");
+    showarray(staff, MAXSTAFF);
+    return 0;
+}
+
+void showarray(const names ar[], int n)
+{
+    int index;
+    for(index = 0; index < n; index++)
+        printf("%d. %s %s\n", index + 1, ar[index].first, ar[index].last);
+}
+
+int mycomp(const void * p1, const void * p2)
+{
+    const names * a1 = (const names *) p1;
+    const names * a2 = (const names *) p2;
+    if (strcmp(a1->first, a2->first) < 0)
+        return -1;
+    if (strcmp(a1->first, a2->first) > 0)
+        return 1;
+
+    if (strcmp(a1->last, a2->last) < 0)
+        return -1;
+    if (strcmp(a1->last, a2->last) > 0)
+        return 1;
+    return 0;
+}
